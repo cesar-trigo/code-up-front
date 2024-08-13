000d702c2b54ed9d45bf42a6405d7467c7a3bed7
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-08-13 17:56:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Layout from "./layout/Layout";
+import ComponentRoutes from "./routes/ComponentRoutes";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <ComponentRoutes />
+      </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+import style from "./Home.module.css";
+
+export default function Home() {
+  return <main className={style.home}>Home</main>;
+}
